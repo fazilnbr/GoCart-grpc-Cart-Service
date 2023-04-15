1fc5f395acf95faaef3b3ab05199360f9ccabb05
fazilnbr <fazilkp2000@gmail.com>
2023-04-15 09:40:11 +0530

    init():folder and file setup
    here intitialize the folder setup and files then inject the dependency using wire

---
+++ pkg/db/connection.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBCart, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
+	return db, dbErr
+}
---
+++ pkg/di/wire.go
@@ -0,0 +1,24 @@
+//go:build wireinject
+// +build wireinject
+
+package di
+
+import (
+	http "github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/api"
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/api/service"
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/config"
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/db"
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/repository"
+	"github.com/fazilnbr/GoCart-grpc-cart-Service/pkg/usecase"
+	"github.com/google/wire"
+)
+
+func InitializeAPI(cfg config.Config) (*http.ServerHTTP, error) {
+	wire.Build(db.ConnectDatabase,
+		repository.NewCartRepository,
+		usecase.NewCartUseCase,
+		service.NewCartService,
+		http.NewServerHTTP)
+
+	return &http.ServerHTTP{}, nil
+}
